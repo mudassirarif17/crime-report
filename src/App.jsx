fe1714a96e42e410e0773fa713882db236ab9c2c
Mudassir <mudassirinoxent@gmail.com>
2024-08-13 00:07:51 +0500

    router is configure in project

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,23 @@ import CrimeReports from './Pages/CrimeReports'
 import About from './Pages/About'
 import Contact from './Pages/Contact'
 import CrimeResponse from './Pages/CrimeResponse'
+import Home from './Pages/Home'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>
-      {/* <SignUp/> */}
-      {/* <Login /> */}
-      {/* <CrimeReports/> */}
-      <CrimeResponse/>
-      {/* <About/> */}
-      {/* <Contact/> */}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/sign-up' element={<SignUp/>}/>
+        <Route path='/sign-in' element={<Login />}/>
+        <Route path='/crime-report' element={<CrimeReports/>}/>
+        <Route path='/crime-response' element={<CrimeResponse/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/about' element={<About/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import logo from "../assets/footerlogo.png";
 import { RxHamburgerMenu } from "react-icons/rx";
+import { Link } from 'react-router-dom';
 
 
 
@@ -22,16 +23,16 @@ const Navbar = () => {
           <img src={logo} alt="" />
         </div>
         <ul className="menu md:flex hidden text-white space-x-6 text-lg cursor-pointer">
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Crime Reports</li>
-          <li>Crime Response's</li>
-          <li>Contact Us</li>
+          <Link to="/">Home</Link>
+          <Link to='/about'>About Us</Link>
+          <Link to='/crime-report'>Crime Reports</Link>
+          <Link to='/crime-response'>Crime Response's</Link>
+          <Link to='/contact'>Contact Us</Link>
         </ul>
 
         <div className="btns hidden md:block">
-          <button className='text-white font-semibold mx-4 text-lg'>Login</button>
-          <button className='text-white font-semibold text-lg px-4 py-1 rounded-lg bg-[#309689]'>Register</button>
+          <Link to='/sign-in' className='text-white font-semibold mx-4 text-lg'>Login</Link>
+          <Link to='/sign-up' className='text-white font-semibold text-lg px-4 py-1 rounded-lg bg-[#309689]'>Register</Link>
         </div>
 
         <div onClick={toggleNavbar} className="burger">
@@ -43,16 +44,16 @@ const Navbar = () => {
       <div id='mbl-menu' className="mbl-menu md:hidden  bg-[#03302D] text-white h-[33vh]">
 
         <ul className='space-y-4 mx-6 text-xl'>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Crime Reports</li>
-          <li>Crime Response's</li>
-          <li>Contact Us</li> 
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/about'>About Us</Link></li>
+          <li><Link to='/crime-report'>Crime Reports</Link></li>
+          <li><Link to='/crime-response'>Crime Response's</Link></li>
+          <li><Link to='/contact'>Contact Us</Link> </li>
         </ul>
 
         <div className="btns mx-6 my-4">
-          <button className='text-white font-semibold text-lg mr-2'>Login</button>
-          <button className='text-white font-semibold text-lg px-4 py-1 rounded-lg bg-[#309689]'>Register</button>
+          <Link to='/sign-in' className='text-white font-semibold text-lg mr-2'>Login</Link>
+          <Link to='/sign-up' className='text-white font-semibold text-lg px-4 py-1 rounded-lg bg-[#309689]'>Register</Link>
         </div>
 
       </div>
