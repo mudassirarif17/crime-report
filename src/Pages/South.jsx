23923b55c578d39c5db1c6f8201ac023b7a4f150
Mudassir <mudassirinoxent@gmail.com>
2024-08-18 11:10:57 +0500

    all excel has been fetched

--- src/Pages/South.jsx
+++ src/Pages/South.jsx
@@ -1,47 +1,47 @@
-import React, { useContext , useEffect } from 'react'
+import React  from 'react'
 import Layout from "./Layout";
-import myContext from '../context/data/myContext';
 import {south} from "../cr-23"
 
 
 const Crime23 = () => {
-  const context = useContext(myContext);
-  const {crimeSouth , southCrime} = context;
-
-  useEffect(() => {
-    crimeSouth();
-}, [southCrime]);
-
-
   return (
     <Layout>
         <div className='w-[90vw] mx-auto my-10'>
   <div className="overflow-x-auto shadow-lg rounded-lg">
     <table className="min-w-full bg-white">
-      <thead className="bg-gray-100">
+    <thead className="bg-gray-100">
         <tr>
-          <th className="py-3 px-4 border-b border-gray-200 text-left text-sm font-semibold text-gray-600">
+          <th className="py-3 text-center px-4 border-b border-gray-200  text-sm font-semibold text-gray-600">
           BANK ROBBERY
           </th>
-          <th className="py-3 px-4 border-b border-gray-200 text-left text-sm font-semibold text-gray-600">
+          <th className="py-3 px-4 border-b border-gray-200 text-center text-sm font-semibold text-gray-600">
             CAR SNATCHING
           </th>
-          <th className="py-3 px-4 border-b border-gray-200 text-left text-sm font-semibold text-gray-600">
+          <th className="py-3 px-4 border-b border-gray-200 text-center text-sm font-semibold text-gray-600">
             DISTRICT
           </th>
-          <th className="py-3 px-4 border-b border-gray-200 text-left text-sm font-semibold text-gray-600">
+          <th className="py-3 px-4 border-b border-gray-200 text-center text-sm font-semibold text-gray-600">
             GANGRAPE
           </th>
-          <th className="py-3 px-4 border-b border-gray-200 text-left text-sm font-semibold text-gray-600">
+          <th className="py-3 px-4 border-b border-gray-200 text-center text-sm font-semibold text-gray-600">
             HIGHYWAY ROBBERY
           </th>
-          <th className="py-3 px-4 border-b border-gray-200 text-left text-sm font-semibold text-gray-600">
+          <th className="py-3 px-4 border-b border-gray-200 text-center text-sm font-semibold text-gray-600">
             MURDER
           </th>
-          <th className="py-3 px-4 border-b border-gray-200 text-left text-sm font-semibold text-gray-600">
+          <th className="py-3 px-4 border-b border-gray-200 text-center text-sm font-semibold text-gray-600">
+            MURDER DURING ROBBERY
+          </th>
+          <th className="py-3 px-4 border-b border-gray-200 text-center text-sm font-semibold text-gray-600">
+            TARGET KILLING
+          </th>
+          <th className="py-3 px-4 border-b border-gray-200 text-center text-sm font-semibold text-gray-600">
+            SUICIDE
+          </th>
+          <th className="py-3 px-4 border-b border-gray-200 text-center text-sm font-semibold text-gray-600">
             MONTH
           </th>
-          <th className="py-3 px-4 border-b border-gray-200 text-left text-sm font-semibold text-gray-600">
+          <th className="py-3 px-4 border-b border-gray-200 text-center text-sm font-semibold text-gray-600">
             YEAR
           </th>
         </tr>
@@ -50,28 +50,37 @@ const Crime23 = () => {
         {
           south.map((data , index)=>(
             <tr className="hover:bg-gray-50">
-          <td className="py-3 px-4 border-b border-gray-200 text-sm text-gray-700">
+          <td className="py-3 px-4 border-b text-center border-gray-200 text-sm text-gray-700">
             {data['BANK ROBBERY']}
           </td>
-          <td className="py-3 px-4 border-b border-gray-200 text-sm text-gray-700">
+          <td className="py-3 px-4 border-b text-center border-gray-200 text-sm text-gray-700">
             {data['CAR SNATCHING']}
           </td>
-          <td className="py-3 px-4 border-b border-gray-200 text-sm text-gray-700">
+          <td className="py-3 px-4 border-b text-center border-gray-200 text-sm text-gray-700">
             {data.District}
           </td>
-          <td className="py-3 px-4 border-b border-gray-200 text-sm text-gray-700 cursor-pointer">
+          <td className="py-3 px-4 border-b text-center border-gray-200 text-sm text-gray-700 cursor-pointer">
           {data['GANG RAPE']}
           </td>
-          <td className="py-3 px-4 border-b border-gray-200 text-sm text-gray-700 cursor-pointer">
+          <td className="py-3 px-4 border-b text-center border-gray-200 text-sm text-gray-700 cursor-pointer">
           {data['HIGH WAY DACOITY/ROBBERY']}
           </td>
-          <td className="py-3 px-4 border-b border-gray-200 text-sm text-gray-700 cursor-pointer">
+          <td className="py-3 px-4 border-b text-center border-gray-200 text-sm text-gray-700 cursor-pointer">
           {data.MURDER}
           </td>
-          <td className="py-3 px-4 border-b border-gray-200 text-sm text-gray-700 cursor-pointer">
+          <td className="py-3 px-4 border-b text-center border-gray-200 text-sm text-gray-700 cursor-pointer">
+          {data['MURDER DURING ROBBERY']}
+          </td>
+          <td className="py-3 px-4 border-b text-center border-gray-200 text-sm text-gray-700 cursor-pointer">
+          {data['MURDER IN FORM OF TARGETED KILLING']}
+          </td>
+          <td className="py-3 px-4 border-b text-center border-gray-200 text-sm text-gray-700 cursor-pointer">
+          {data['SUCIDE BOMBING/ BOMB BLAST']}
+          </td>
+          <td className="py-3 px-4 border-b text-center border-gray-200 text-sm text-gray-700 cursor-pointer">
           {data.Month}
           </td>
-          <td className="py-3 px-4 border-b border-gray-200 text-sm text-gray-700 cursor-pointer">
+          <td className="py-3 px-4 border-b text-center border-gray-200 text-sm text-gray-700 cursor-pointer">
           {data.Year}
           </td>
         </tr>
--- src/Pages/West.jsx
+++ src/Pages/West.jsx
@@ -1,10 +1,96 @@
 import React from 'react'
+import Layout from "./Layout";
+import {west} from "../cr-23"
 
 const West = () => {
   return (
-    <div>
-      <h1>West</h1>
-    </div>
+    <Layout>
+        <div className='w-[90vw] mx-auto my-10'>
+  <div className="overflow-x-auto shadow-lg rounded-lg">
+    <table className="min-w-full bg-white">
+      <thead className="bg-gray-100">
+        <tr>
+          <th className="py-3 text-center px-4 border-b border-gray-200  text-sm font-semibold text-gray-600">
+          BANK ROBBERY
+          </th>
+          <th className="py-3 px-4 border-b border-gray-200 text-center text-sm font-semibold text-gray-600">
+            CAR SNATCHING
+          </th>
+          <th className="py-3 px-4 border-b border-gray-200 text-center text-sm font-semibold text-gray-600">
+            DISTRICT
+          </th>
+          <th className="py-3 px-4 border-b border-gray-200 text-center text-sm font-semibold text-gray-600">
+            GANGRAPE
+          </th>
+          <th className="py-3 px-4 border-b border-gray-200 text-center text-sm font-semibold text-gray-600">
+            HIGHYWAY ROBBERY
+          </th>
+          <th className="py-3 px-4 border-b border-gray-200 text-center text-sm font-semibold text-gray-600">
+            MURDER
+          </th>
+          <th className="py-3 px-4 border-b border-gray-200 text-center text-sm font-semibold text-gray-600">
+            MURDER DURING ROBBERY
+          </th>
+          <th className="py-3 px-4 border-b border-gray-200 text-center text-sm font-semibold text-gray-600">
+            TARGET KILLING
+          </th>
+          <th className="py-3 px-4 border-b border-gray-200 text-center text-sm font-semibold text-gray-600">
+            SUICIDE
+          </th>
+          <th className="py-3 px-4 border-b border-gray-200 text-center text-sm font-semibold text-gray-600">
+            MONTH
+          </th>
+          <th className="py-3 px-4 border-b border-gray-200 text-center text-sm font-semibold text-gray-600">
+            YEAR
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          west.map((data , index)=>(
+            <tr className="hover:bg-gray-50">
+          <td className="py-3 px-4 border-b text-center border-gray-200 text-sm text-gray-700">
+            {data['BANK ROBBERY']}
+          </td>
+          <td className="py-3 px-4 border-b text-center border-gray-200 text-sm text-gray-700">
+            {data['CAR SNATCHING']}
+          </td>
+          <td className="py-3 px-4 border-b text-center border-gray-200 text-sm text-gray-700">
+            {data.District}
+          </td>
+          <td className="py-3 px-4 border-b text-center border-gray-200 text-sm text-gray-700 cursor-pointer">
+          {data['GANG RAPE']}
+          </td>
+          <td className="py-3 px-4 border-b text-center border-gray-200 text-sm text-gray-700 cursor-pointer">
+          {data['HIGH WAY DACOITY/ROBBERY']}
+          </td>
+          <td className="py-3 px-4 border-b text-center border-gray-200 text-sm text-gray-700 cursor-pointer">
+          {data.MURDER}
+          </td>
+          <td className="py-3 px-4 border-b text-center border-gray-200 text-sm text-gray-700 cursor-pointer">
+          {data['MURDER DURING ROBBERY']}
+          </td>
+          <td className="py-3 px-4 border-b text-center border-gray-200 text-sm text-gray-700 cursor-pointer">
+          {data['MURDER IN FORM OF TARGETED KILLING']}
+          </td>
+          <td className="py-3 px-4 border-b text-center border-gray-200 text-sm text-gray-700 cursor-pointer">
+          {data['SUCIDE BOMBING/ BOMB BLAST']}
+          </td>
+          <td className="py-3 px-4 border-b text-center border-gray-200 text-sm text-gray-700 cursor-pointer">
+          {data.Month}
+          </td>
+          <td className="py-3 px-4 border-b text-center border-gray-200 text-sm text-gray-700 cursor-pointer">
+          {data.Year}
+          </td>
+        </tr>
+          ))
+        }
+      </tbody>
+    </table>
+  </div>
+</div>
+
+    </Layout>
   )
 }
 
