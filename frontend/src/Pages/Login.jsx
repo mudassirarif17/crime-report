b5e22a32467b92b955663a53d8c875ce2193923b
DeveloperMudassir <76750653+mudassirarif17@users.noreply.github.com>
2024-08-20 21:04:04 +0500

    Update Login.jsx
    also added in login

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const res = await fetch(`http://localhost:5000/api/auth/login`, {
+      const res = await fetch(`crime-report-three.vercel.app/api/auth/login`, {
         method: "POST",
         headers: {
           'Content-Type': "application/json"
